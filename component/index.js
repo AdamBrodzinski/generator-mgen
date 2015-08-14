f1f4a39254626963360a9b92803296bac5927a40
Adam Brodzinski <adambrodzinski@gmail.com>
2015-08-13 23:11:48 -0400

    adds conditional env
    now spits out different boilerplates for diff platforms

--- component/index.js
+++ component/index.js
@@ -5,7 +5,9 @@ module.exports = generators.Base.extend({
     generators.Base.apply(this, arguments);
     this.argument('name', { type: String, required: true });
     this.ext = this.config.get('componentExtension') || '.jsx';
-    this.log(this.config.getAll());
+    //this.log(this.config.getAll());
+    //this.log(this.arguments);
+    this.log('Creating component files...\n');
   },
 
   getPaths: function getPaths() {
@@ -14,12 +16,21 @@ module.exports = generators.Base.extend({
     this.destPath = this.rootPath + this.nameLower + '/' + this.name + this.ext;
   },
 
+  getComponent: function getComponent() {
+    if (this.config.get('env') === 'meteor') {
+      this.compPath = 'comp_mt.js';
+    } else if (this.config.get('env') === 'react-native') {
+      this.compPath = 'comp_rn.js';
+    } else {
+      this.compPath = 'comp.js';
+    }
+  },
+
   writeFile: function writeFile() {
     this.fs.copyTpl(
-      this.templatePath('comp.js'),
+      this.templatePath(this.compPath),
       this.destinationPath(this.destPath),
       { name: this.name }
     );
-    this.log('Creating component files...\n');
   },
 });
